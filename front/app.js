604d26745aea9eaf3ac9f4fc6ff4ec4890166f29
enta0701 <endo.takuya.0701@gmail.com>
2018-12-17 16:49:21 +0900

    feat: Dockernize

--- front/app.js
+++ front/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const caller = require('grpc-caller');
 const PROTO_PATH = __dirname + '/../article/article.proto';
-const client = caller('localhost:50051', PROTO_PATH, 'ArticleService');
+const SERVER_HOST = process.env.SERVER_HOST || 'localhost';
+const client = caller(`${SERVER_HOST}:50051`, PROTO_PATH, 'ArticleService');
 const app = express();
 const PORT = 3000;
 
