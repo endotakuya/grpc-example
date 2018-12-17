81a519f8b219d14499019f3ea3dadcc65a4979ee
enta0701 <endo.takuya.0701@gmail.com>
2018-12-17 18:42:30 +0900

    feat: Post data

--- front/app.js
+++ front/app.js
@@ -5,11 +5,37 @@ const SERVER_HOST = process.env.SERVER_HOST || 'localhost';
 const client = caller(`${SERVER_HOST}:50051`, PROTO_PATH, 'ArticleService');
 const app = express();
 const PORT = 3000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    client.first({}, (err, response) => {
-        console.log(response);
-        res.send(response.content)
+    client.first({}, (_err, response) => {
+        res.render('index', response);
+    });
+});
+
+app.get('/new', (req, res) => {
+    res.render('new');
+});
+
+app.post('/new', (req, res) => {
+    let data = {
+        title: req.body.title,
+        content: req.body.content,
+        status: parseInt(req.body.status, 10)
+    };
+    console.log(data);
+    client.post(data, (err, _response) => {
+        if (err == null) {
+            res.redirect('/');
+        } else {
+            res.redirect('/new');
+        }
     });
 });
 
--- server.go
+++ server.go
@@ -2,22 +2,53 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
 
 	pb "github.com/endotakuya/grpc-exsample/article"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 const (
-	port = ":50051"
+	port   = ":50051"
+	db     = "grpc_example"
+	dbUser = "root"
+	dbPass = ""
+	dbHost = "db"
+	dbPort = 3306
 )
 
 type server struct{}
 
 func (s *server) First(ctx context.Context, in *pb.Empty) (*pb.Article, error) {
-	return &pb.Article{Id: 1, Title: "タイトル", Content: "本文", Status: pb.Article_PUBLISH}, nil
+	db, _ := initDb()
+	defer db.Close()
+
+	article := pb.Article{}
+	dist := []interface{}{&article.Id, &article.Title, &article.Content, &article.Status}
+	err := db.QueryRow("SELECT * FROM articles ORDER BY id DESC LIMIT 1").Scan(dist...)
+	return &article, err
+}
+
+func (s *server) Post(ctx context.Context, in *pb.Article) (*pb.Empty, error) {
+	db, _ := initDb()
+	defer db.Close()
+
+	stmtIns, err := db.Prepare(fmt.Sprintf("INSERT INTO %s (title, content, status) VALUES (?, ?, ?)", "articles"))
+	defer stmtIns.Close()
+
+	_, err = stmtIns.Exec(in.Title, in.Content, in.Status)
+	return &pb.Empty{}, err
+}
+
+func initDb() (*sql.DB, error) {
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", dbUser, dbPass, dbHost, dbPort, db)
+	return sql.Open("mysql", dataSourceName)
 }
 
 func main() {
